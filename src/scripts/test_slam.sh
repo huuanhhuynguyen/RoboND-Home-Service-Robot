0126064fe4d11919a71a32df50ef4ad42cb2e765
huuanhhuynguyen <huuanhhuynguyen@gmail.com>
2020-05-31 17:33:21 +0200

    Add gmapping_demo.launch

--- src/scripts/test_slam.sh
+++ src/scripts/test_slam.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 xterm  -e  " roslaunch turtlebot turtlebot_world.launch " &
 sleep 3
+xterm  -e  " roslaunch turtlebot gmapping_demo.launch " &
+sleep 3
 xterm  -e  " roslaunch turtlebot view_navigation.launch " &
 sleep 3
 xterm  -e  " rosrun teleop_twist_keyboard teleop_twist_keyboard.py " &
