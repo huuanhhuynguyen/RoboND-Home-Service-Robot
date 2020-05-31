7945ebd5dde51d6226d509f723c540ae38c81b51
huuanhhuynguyen <huuanhhuynguyen@gmail.com>
2020-05-31 16:30:29 +0200

    Launch rviz with a saved config file.

--- src/scripts/test_slam.sh
+++ src/scripts/test_slam.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 xterm  -e  " roslaunch turtlebot turtlebot_world.launch " &
 sleep 3
+xterm  -e  " roslaunch turtlebot view_navigation.launch " &
+sleep 3
 xterm  -e  " rosrun teleop_twist_keyboard teleop_twist_keyboard.py " &
 sleep 3
