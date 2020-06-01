64f5825b9bd1360c281cf64cdab42d5c8ba07cc5
huuanhhuynguyen <huuanhhuynguyen@gmail.com>
2020-06-01 19:46:38 +0200

    Launch add_markers before pick_objects

--- src/scripts/home_service.sh
+++ src/scripts/home_service.sh
@@ -5,7 +5,7 @@ xterm  -e  " roslaunch turtlebot amcl_demo.launch " &
 sleep 3
 xterm  -e  " roslaunch turtlebot view_navigation.launch " &
 sleep 3
-xterm  -e  " rosrun pick_objects pick_objects_node " &
-sleep 3
 xterm  -e  " rosrun add_markers add_markers_node " &
+sleep 3
+xterm  -e  " rosrun pick_objects pick_objects_node " &
 sleep 3
