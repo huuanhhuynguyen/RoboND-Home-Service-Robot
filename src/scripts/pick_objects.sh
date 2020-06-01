11fb74379fa281cd8f612db9955c0fa77cc37f15
huuanhhuynguyen <huuanhhuynguyen@gmail.com>
2020-06-01 12:10:11 +0200

    Add pick_objects.sh

---
+++ src/scripts/pick_objects.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+xterm  -e  " roslaunch turtlebot turtlebot_world.launch " &
+sleep 3
+xterm  -e  " roslaunch turtlebot amcl_demo.launch " &
+sleep 3
+xterm  -e  " roslaunch turtlebot view_navigation.launch " &
+sleep 3
+xterm  -e  " rosrun pick_objects pick_objects_node " &
+sleep 3
