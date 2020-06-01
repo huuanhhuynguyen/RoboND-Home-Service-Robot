f9e7561781d756a51e8afd778d399ee8e55374a4
huuanhhuynguyen <huuanhhuynguyen@gmail.com>
2020-06-01 16:15:49 +0200

    Add home_service.sh

---
+++ src/scripts/home_service.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+xterm  -e  " roslaunch turtlebot turtlebot_world.launch " &
+sleep 3
+xterm  -e  " roslaunch turtlebot amcl_demo.launch " &
+sleep 3
+xterm  -e  " roslaunch turtlebot view_navigation.launch " &
+sleep 3
+xterm  -e  " rosrun pick_objects pick_objects_node " &
+sleep 3
+xterm  -e  " rosrun add_markers add_markers_node " &
+sleep 3
